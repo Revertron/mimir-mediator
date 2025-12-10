92e89bd4b6c0e8c02177fca3af4cb6f70736d64a
Revertron <rev@revertron.com>
2025-12-10 13:41:03 +0100

    Fixed sqlite concurrency error.

--- mediator.go
+++ mediator.go
@@ -219,7 +219,11 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	if _, err := db.Exec(`PRAGMA journal_mode=WAL; PRAGMA foreign_keys=ON;`); err != nil {
+	// Configure connection pool for SQLite
+	db.SetMaxOpenConns(1)
+	db.SetMaxIdleConns(1)
+	db.SetConnMaxLifetime(0)
+	if _, err := db.Exec(`PRAGMA journal_mode=WAL; PRAGMA foreign_keys=ON; PRAGMA busy_timeout=5000;`); err != nil {
 		log.Fatal(err)
 	}
 	if err := initGlobalSchema(db); err != nil {
